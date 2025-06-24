78ef7273fba17816071a1b7e38fc2230ad0577b1
Sumanth Lingappa <42572246+sumanth-lingappa@users.noreply.github.com>
2025-06-24 07:16:30 +0530

    working: socketio chat app stage1
    Signed-off-by: Sumanth Lingappa <42572246+sumanth-lingappa@users.noreply.github.com>

---
+++ socketio-chat-app/README.md
@@ -0,0 +1,9 @@
+# SocketIO chat app
+
+```bash
+docker compose up --build
+```
+
+Open two tabs with <http:localhost:5050>
+
+![alt text](./assets/stage1-chat-app.png)
---
+++ socketio-chat-app/app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins='*', async_mode="eventlet")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    print(f"Received message: {msg}")
+    send(msg, broadcast=True)
+
+if __name__ == "__main__":
+    socketio.run(app, host='0.0.0.0', port=5000)
