d0368ae861c94ed5991ad6efc7a3b07c56bb9ed8
Sumanth Lingappa <42572246+sumanth-lingappa@users.noreply.github.com>
2025-07-12 14:21:08 +0530

    simple TCP server listening to one client for one message
    Signed-off-by: Sumanth Lingappa <42572246+sumanth-lingappa@users.noreply.github.com>

---
+++ socket-programming/simple/client.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define SERVER_PORT 8080
+#define SERVER_ADDR "127.0.0.1"
+#define BUF_SIZE 1024
+
+int main()
+{
+    // socket
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_fd < 0)
+    {
+        perror("client couldn't create socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // prepare the server details to connect to
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    // convert human readable IP to binary format
+    if (inet_pton(AF_INET, SERVER_ADDR, &server_addr.sin_addr) <= 0)
+    {
+        perror("Invalid server address to connect");
+        close(client_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // connect
+    // Connect to the server
+    if (connect(client_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Couldn't connect to server");
+        close(client_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // write
+    const char *msg = "Hello from client!";
+    write(client_fd, msg, strlen(msg));
+
+    // read
+    char buffer[BUF_SIZE] = {0};
+    read(client_fd, buffer, sizeof(buffer));
+    printf("Server replied: %s\n", buffer);
+
+    // close
+    close(client_fd);
+    return 0;
+}
---
+++ socket-programming/simple/server.c
@@ -0,0 +1,77 @@
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <stdio.h>
+
+#define PORT 8080     // Server listens on this port
+#define BUF_SIZE 1024 // size of the read/write buffer
+
+int main()
+{
+    int server_fd;
+
+    // Create a TCP socket using IPv4. (AF_INET) and stream sockets (SOCK_STEAM). For UDP, it will be SOCK_DGRAM
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1)
+    {
+        perror("socket failed!");
+        exit(EXIT_FAILURE);
+    }
+
+    // Prepare the sockaddr_in structure with the server address info
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));    // Clear the memory to avoid garbage
+    addr.sin_family = AF_INET;         // IPv4
+    addr.sin_addr.s_addr = INADDR_ANY; // Accept connection on any local IP
+    addr.sin_port = htons(PORT);       // Convert port to network byte order
+
+    // Bind the socket to the IP address and port
+    int bind_ret = bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)); // typecast the sockaddr_in addr to the generic `sockaddr` struct which bind expects
+    if (bind_ret < 0)
+    {
+        perror("bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Inform the OS that the server is not going to connect to anyone. It wants to receive connection requests from clients. Please put the server in listen mode
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("Couldn't listen!");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d...\n", PORT);
+
+    // Wait for the client to connect
+    // int client_fd = accept(server_fd, NULL, NULL); // I will accept an incoming connection, but I dont care who it is or where they are from. Don't give me the client's address
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+
+    // convert and print the client IP and port
+    char client_ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
+    printf("Got connection from %s:%d", client_ip, ntohs(client_addr.sin_port));
+
+    // Client has connected. Liste to what client has to say
+    char buffer[BUF_SIZE] = {0};
+    ssize_t bytes = read(client_fd, buffer, BUF_SIZE);
+    if (bytes > 0)
+    {
+        printf("Received from client: %s\n", buffer);
+    }
+
+    // Respond to the client, eg: echo back the client's request
+    int write_bytes = write(client_fd, buffer, strlen(buffer));
+    printf("Writtein %d bytes", write_bytes);
+
+    // close the sessions
+    close(client_fd);
+    close(server_fd);
+
+    return 0;
+}
